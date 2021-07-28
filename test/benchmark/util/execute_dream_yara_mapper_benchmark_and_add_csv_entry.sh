1697e4f8b8fde36ba66a64d77b2c49b51232778d
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2021-07-28 13:43:12 +0200

    ensure python3

--- test/benchmark/util/execute_dream_yara_mapper_benchmark_and_add_csv_entry.sh
+++ test/benchmark/util/execute_dream_yara_mapper_benchmark_and_add_csv_entry.sh
@@ -12,6 +12,7 @@ BENCHMARK_UTIL_DIR="$1"
 BINARY_DIR="$2"
 TMP_DIR="$3"
 BENCHMARK_DIR="$4"
+python3=$(command -v python3)
 
 threads=4
 error_rate=0.03 # 0$(bc -l <<< "($ERRORS+1)/$READ_LENGTH")
@@ -28,10 +29,10 @@ error_rate=0.03 # 0$(bc -l <<< "($ERRORS+1)/$READ_LENGTH")
         $BENCHMARK_DIR/reads_e2_100.fastq \
         &> $TMP_DIR/yara_mapper.log
 
-python $BENCHMARK_UTIL_DIR/parse_time_log.py $TMP_DIR/yara_mapper.time > $TMP_DIR/yara_mapper.time.json
-python $BENCHMARK_UTIL_DIR/parse_yara_mapper_log.py $TMP_DIR/yara_mapper.log > $TMP_DIR/yara_mapper.log.json
+$python3 $BENCHMARK_UTIL_DIR/parse_time_log.py $TMP_DIR/yara_mapper.time > $TMP_DIR/yara_mapper.time.json
+$python3 $BENCHMARK_UTIL_DIR/parse_yara_mapper_log.py $TMP_DIR/yara_mapper.log > $TMP_DIR/yara_mapper.log.json
 
-python $BENCHMARK_UTIL_DIR/add_benchmark_csv_entry.py \
+$python3 $BENCHMARK_UTIL_DIR/add_benchmark_csv_entry.py \
     $TMP_DIR/yara_mapper.time.json \
     $TMP_DIR/yara_mapper.log.json \
     --append-to-csv $BENCHMARK_DIR/benchmark_csv_file.csv
